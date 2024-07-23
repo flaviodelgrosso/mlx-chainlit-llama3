9f8f1bb87a85b7cb38624b1442d06d70f67341cd
Flavio Del Grosso <flaviodelgrosso@live.it>
2024-07-23 22:28:20 +0200

    Update server.sh to use new Llama 3.1

--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-python -m mlx_lm.server --model mlx-community/Meta-Llama-3-8B-Instruct-4bit --log-level DEBUG
+python -m mlx_lm.server --model mlx-community/Meta-Llama-3.1-8B-Instruct-4bit --log-level DEBUG
